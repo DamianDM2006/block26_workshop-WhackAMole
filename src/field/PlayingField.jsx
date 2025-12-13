9bb7477ac868faacf4a201c16df9acd68e8ac597
Damian Martinez <DCAM.DAMIAN.DM@GMAIL.COM>
2025-12-12 22:15:31 -0700

    holeArray created w/ default is empty: true

--- src/field/PlayingField.jsx
+++ src/field/PlayingField.jsx
@@ -5,15 +5,23 @@ const PlayingField = () => {
   const columns =3;
   const holes = rows * columns;
 
-  
+  const holeArray = Array.from({ length: holes }, (_, index) => 
+    ({id: index + 1,
+      isEmpty: true
+    }));
+  console.log(holeArray);
 
-  return (
-    <figure className="field">
-      <img
-        className="hole"
-        src="public/hole.png" />
-    </figure>
-  )
+
+/** hole{ (type object)
+ *    holeNo: which hole is this
+ *    holeType: empty or mole (default is empty)
+ *      if(holeType = mole) {
+ *         amount of time mole is up
+ *          onClick to add to score and empty hole
+ *          }
+} */
+
+  return <img className="hole" />
 };
 
 export default PlayingField;
